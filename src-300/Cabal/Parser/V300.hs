12c101bde7842b15424200320535fc02c2c5bdb9
Alex Biehl <alex.biehl@target.com>
2020-03-06 21:22:12 +0100

    Hook up Cabal-3.0 into parsing pipeline

---
+++ src-300/Cabal/Parser/V300.hs
@@ -0,0 +1,13 @@
+module Cabal.Parser.V300
+  ( module X
+  ) where
+
+import           Distribution.PackageDescription             as X
+import           Distribution.PackageDescription.Check       as X
+import           Distribution.PackageDescription.Parsec      as X
+import           Distribution.PackageDescription.PrettyPrint as X
+import           Distribution.Parsec.Error                   as X
+import           Distribution.Parsec.Position                as X
+import           Distribution.Text                           as X
+import           Distribution.Verbosity                      as X
+import           Distribution.Version                        as X
---
+++ src-300/Paths_Cabal.hs
@@ -0,0 +1,6 @@
+module Paths_Cabal where
+
+import           Data.Version
+
+version :: Version
+version = makeVersion [3,0,0,0]
