e768ac7628bbec0c756402771113416b0240960a
Herbert Valerio Riedel <hvr@gnu.org>
2020-05-02 21:45:15 +0200

    Vendor in Cabal-2.4.1
    Corresponds to haskell/Cabal@b7cd00bd75e379abee79a8ae5ac049b9a10ac0e8

---
+++ src-204/Cabal/Parser/V204.hs
@@ -0,0 +1,14 @@
+module Cabal.Parser.V204
+    ( module X
+    ) where
+
+-- import           Distribution.InstalledPackageInfo           as X (PError (..))
+import           Distribution.PackageDescription             as X
+import           Distribution.PackageDescription.Check       as X
+import           Distribution.PackageDescription.Parsec      as X
+import           Distribution.PackageDescription.PrettyPrint as X
+import           Distribution.Parsec.Common                  as X
+import           Distribution.ParseUtils                     as X (locatedErrorMsg)
+import           Distribution.Text                           as X
+import           Distribution.Verbosity                      as X
+import           Distribution.Version                        as X
---
+++ src-204/Paths_Cabal.hs
@@ -0,0 +1,6 @@
+module Paths_Cabal where
+
+import           Data.Version
+
+version :: Version
+version = makeVersion [2,4,1,0]
